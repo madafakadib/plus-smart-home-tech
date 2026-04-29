33d1d17ac75a06e97e9bb9067165c90f75b680dc
Denis <dessitnov22@yandex.ru>
2026-04-29 15:57:31 +0300

    3-aggregator

---
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/AggregationStarter.java
@@ -0,0 +1,103 @@
+package ru.yandex.practicum.aggregator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.kafka.deserializer.SensorEventDeserializer;
+import ru.yandex.practicum.kafka.serializer.GeneralAvroSerializer;
+import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Класс AggregationStarter, ответственный за запуск агрегации данных.
+ */
+@Slf4j
+@Component
+public class AggregationStarter {
+
+    private final KafkaConsumer<String, SensorEventAvro> consumer;
+    private final KafkaProducer<String, SensorsSnapshotAvro> producer;
+    private final SnapshotService snapshotService;
+
+    public AggregationStarter(KafkaConsumer<String, SensorEventAvro> consumer, KafkaProducer<String, SensorsSnapshotAvro> producer, SnapshotService snapshotService) {
+        this.consumer = consumer;
+        this.producer = producer;
+        this.snapshotService = snapshotService;
+
+        Properties configConsum = new Properties();
+        configConsum.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configConsum.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configConsum.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorEventDeserializer.class);
+
+        Properties configProduc = new Properties();
+        configProduc.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProduc.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProduc.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class);
+
+    }
+
+
+    /**
+     * Метод для начала процесса агрегации данных.
+     * Подписывается на топики для получения событий от датчиков, 
+     * формирует снимок их состояния и записывает в кафку.
+     */
+    public void start() {
+        try {
+            consumer.subscribe(List.of("telemetry.sensors.v1"));
+            // ... подготовка к обработке данных ...
+            // ... например, подписка на топик ...
+
+            // Цикл обработки событий
+            while (true) {
+                ConsumerRecords<String, SensorEventAvro> records = consumer.poll(Duration.ofMillis(5000));
+                for (ConsumerRecord<String, SensorEventAvro> record : records) {
+                    // Вызываем твою бизнес-логику из SnapshotService
+                    snapshotService.updateState(record.value())
+                            .ifPresent(snapshot -> {
+                                producer.send(new ProducerRecord<>("telemetry.snapshots.v1", snapshot.getHubId(), snapshot));
+                            });
+                    // ... реализация цикла опроса ...
+                    // ... и обработка полученных данных ...
+                }
+                consumer.commitSync();
+            }
+
+        } catch (WakeupException ignored) {
+            // игнорируем - закрываем консьюмер и продюсер в блоке finally
+        } catch (Exception e) {
+            log.error("Ошибка во время обработки событий от датчиков", e);
+        } finally {
+
+            try {
+                // Перед тем, как закрыть продюсер и консьюмер, нужно убедиться,
+                // что все сообщения, лежащие в буффере, отправлены и
+                // все оффсеты обработанных сообщений зафиксированы
+
+                // здесь нужно вызвать метод продюсера для сброса данных в буффере
+                // здесь нужно вызвать метод консьюмера для фиксации смещений
+                producer.flush();
+                consumer.commitSync();
+
+            } finally {
+                log.info("Закрываем консьюмер");
+                consumer.close();
+                log.info("Закрываем продюсер");
+                producer.close();
+            }
+        }
+    }
+}
