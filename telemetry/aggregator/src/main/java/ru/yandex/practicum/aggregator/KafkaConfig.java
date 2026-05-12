27ab173e3cd9092926629cb7ef0ade6dddbce06a
Denis <dessitnov22@yandex.ru>
2026-05-12 22:57:13 +0300

    5-config-server

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/KafkaConfig.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/KafkaConfig.java
@@ -1,10 +1,14 @@
 package ru.yandex.practicum.aggregator;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.yandex.practicum.kafka.deserializer.SensorEventDeserializer;
+import ru.yandex.practicum.kafka.serializer.GeneralAvroSerializer;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 
@@ -27,11 +31,13 @@ public class KafkaConfig {
 
     @Bean
     public KafkaConsumer<String, SensorEventAvro> kafkaConsumer(Properties consumerProperties) {
+        consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorEventDeserializer.class);
         return new KafkaConsumer<>(consumerProperties);
     }
 
     @Bean
     public KafkaProducer<String, SensorsSnapshotAvro> kafkaProducer(Properties producerProperties) {
+        producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class);
         return new KafkaProducer<>(producerProperties);
     }
 }
