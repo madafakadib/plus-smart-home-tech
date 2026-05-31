5fcfd8a865810979ec45c12c0319cf3cb894d2e5
Denis <dessitnov22@yandex.ru>
2026-05-31 17:01:38 +0300

    7-spring-cloud

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/KafkaConfig.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/KafkaConfig.java
@@ -4,7 +4,8 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.yandex.practicum.kafka.deserializer.SensorEventDeserializer;
@@ -18,26 +19,21 @@ import java.util.Properties;
 public class KafkaConfig {
 
     @Bean
-    @ConfigurationProperties(prefix = "aggregator.kafka.consumer.properties")
-    public Properties consumerProperties() {
-        return new Properties();
+    public KafkaConsumer<String, SensorEventAvro> kafkaConsumer() {
+        Properties config = new Properties();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "aggregator-group");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorEventDeserializer.class);
+        return new KafkaConsumer<>(config);
     }
 
     @Bean
-    @ConfigurationProperties(prefix = "aggregator.kafka.producer.properties")
-    public Properties producerProperties() {
-        return new Properties();
-    }
-
-    @Bean
-    public KafkaConsumer<String, SensorEventAvro> kafkaConsumer(Properties consumerProperties) {
-        consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorEventDeserializer.class);
-        return new KafkaConsumer<>(consumerProperties);
-    }
-
-    @Bean
-    public KafkaProducer<String, SensorsSnapshotAvro> kafkaProducer(Properties producerProperties) {
-        producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class);
-        return new KafkaProducer<>(producerProperties);
+    public KafkaProducer<String, SensorsSnapshotAvro> kafkaProducer() {
+        Properties config = new Properties();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class);
+        return new KafkaProducer<>(config);
     }
 }
--- telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/service/KafkaConfig.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/service/KafkaConfig.java
@@ -1,9 +1,12 @@
 package ru.yandex.practicum.analyzer.service;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.yandex.practicum.kafka.deserializer.HubEventDeserializer;
+import ru.yandex.practicum.kafka.deserializer.SnapshotDeserializer;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 
@@ -13,25 +16,25 @@ import java.util.Properties;
 public class KafkaConfig {
 
     @Bean
-    @ConfigurationProperties(prefix = "analyzer.kafka.hubs.properties")
-    public Properties hubConsumerProperties() {
-        return new Properties();
+    public KafkaConsumer<String, HubEventAvro> hubEventConsumer() {
+        Properties config = new Properties();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "analyzer-hubs-group");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, HubEventDeserializer.class);
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        return new KafkaConsumer<>(config);
     }
 
     @Bean
-    @ConfigurationProperties(prefix = "analyzer.kafka.snapshots.properties")
-    public Properties snapshotConsumerProperties() {
-        return new Properties();
-    }
-
-    @Bean
-    public KafkaConsumer<String, HubEventAvro> hubEventConsumer(Properties hubConsumerProperties) {
-        return new KafkaConsumer<>(hubConsumerProperties);
-    }
-
-    @Bean
-    public KafkaConsumer<String, SensorsSnapshotAvro> snapshotConsumer(Properties snapshotConsumerProperties) {
-        return new KafkaConsumer<>(snapshotConsumerProperties);
+    public KafkaConsumer<String, SensorsSnapshotAvro> snapshotConsumer() {
+        Properties config = new Properties();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "analyzer-snapshots-group");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SnapshotDeserializer.class);
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        return new KafkaConsumer<>(config);
     }
 
 }
